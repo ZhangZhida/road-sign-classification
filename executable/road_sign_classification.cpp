ac3c38bc5fb5197542080d42819251e1e0e4ff0a
Zhida <zhangzhida822@outlook.com>
2018-04-04 17:49:39 +0800

    read data

--- executable/road_sign_classification.cpp
+++ executable/road_sign_classification.cpp
@@ -27,10 +27,11 @@ using namespace dlib;
 void loadImages(const string& folder_name, 
     std::vector<matrix<rgb_pixel> > & training_images,
     std::vector<unsigned long>& training_labels, 
-    std::vector<array2d<rgb_pixel> > & testing_images,
+    std::vector<matrix<rgb_pixel> > & testing_images,
     std::vector<unsigned long>& testing_labels);
 int getdir (string dir, std::vector<string> &files);
-void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images);
+void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images, 
+    std::vector<unsigned long>& dlib_labels, unsigned long label);
 
 
 
@@ -38,15 +39,15 @@ void loadImages(const string& folder_name,
     
     std::vector<matrix<rgb_pixel> > & training_images,
     std::vector<unsigned long>& training_labels, 
-    std::vector<array2d<rgb_pixel> > & testing_images,
+    std::vector<matrix<rgb_pixel> > & testing_images,
     std::vector<unsigned long>& testing_labels) {
 
     std::vector<string> files = std::vector<string>();
     
     string train_positive_folder = folder_name + "/train/1";
     string train_negative_folder = folder_name + "/train/0";
-    string test_positive_folder  = folder_name + "test/1";
-    string test_negative_folder  = folder_name + "test/0";
+    string test_positive_folder  = folder_name + "/val/1";
+    string test_negative_folder  = folder_name + "/val/0";
 
     std::vector<string> train_positive_files = std::vector<string>();
     std::vector<string> train_negative_files = std::vector<string>();
@@ -54,9 +55,14 @@ void loadImages(const string& folder_name,
     std::vector<string> test_negative_files = std::vector<string>();
     
     training_images = std::vector<matrix<rgb_pixel> >();
-    loadImageFileIntoDLibImages_one_dir(train_positive_folder, training_images);
+    loadImageFileIntoDLibImages_one_dir(train_positive_folder, training_images, training_labels, 1);
+    loadImageFileIntoDLibImages_one_dir(train_negative_folder, training_images, training_labels, 0);
 
-    cout << training_images.size() <<endl;
+    testing_images = std::vector<matrix<rgb_pixel> >();
+    loadImageFileIntoDLibImages_one_dir(test_positive_folder, testing_images, testing_labels, 1);
+    loadImageFileIntoDLibImages_one_dir(test_negative_folder, testing_images, testing_labels, 0);  
+
+
 
     // train_positive
     // getdir(train_positive_folder, train_positive_files);
@@ -75,12 +81,15 @@ void loadImages(const string& folder_name,
     // }
 }
 
-void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images) {
+void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images,
+     std::vector<unsigned long>& dlib_labels, unsigned long label) {
 
     std::vector<string> files = std::vector<string>();
     getdir(folder, files);
 
-    dlib_images.resize(files.size());
+    // dlib_images.resize(files.size());
+    // dlib_images.resize(files.size());
+    
 
     for(int i=0; i<files.size(); i++) {
 
@@ -91,7 +100,7 @@ void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_p
             
             cv::Mat cv_img = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
             if (cv_img.empty()) {
-                cout << "failed reading image!" << endl;
+                //cout << "failed reading image! The file name is " << filename << endl;
                 continue;
             }
             dlib::cv_image<dlib::rgb_pixel> tmp_img(cv_img);
@@ -100,9 +109,10 @@ void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_p
 
             // load_image(img, filename);
             // dlib_images[i].sets_size(img.nr(), img.nc());
-            cout << files[i].c_str() << dlib_img.size() << endl;
+            //cout << files[i].c_str() << dlib_img.size() << endl;
             // assign_all_pixels(dlib_images[i], img.data);
             dlib_images.push_back(dlib_img);
+            dlib_labels.push_back(label);
         }
         catch (exception& e)
         {
@@ -110,7 +120,8 @@ void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_p
         }
     }
 
-    cout << "size is " << dlib_images.size() << endl;
+    cout << "images size is " << dlib_images.size() << endl;
+    cout << "labels size is " << dlib_labels.size() << endl;
 
 
 
@@ -138,10 +149,13 @@ int main() {
     // inputs
     std::vector<matrix<rgb_pixel> > training_images;
     std::vector<unsigned long>        training_labels;
-    std::vector<array2d<rgb_pixel> > testing_images;
+    std::vector<matrix<rgb_pixel> > testing_images;
     std::vector<unsigned long>      testing_labels;
 
     loadImages(input_dir, training_images, training_labels, testing_images, testing_labels);
+
+    cout << training_images.size() << endl;
+    cout << training_labels.size() << endl;
 
     return 0;
 }
