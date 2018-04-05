230ae9b489315e4086b52df8ee9634c348f54d64
Zhida Zhang <zhangzhida822@outlook.com>
2018-04-05 20:40:58 +0800

    some example from official dlib

--- executable/road_sign_classification.cpp
+++ executable/road_sign_classification.cpp
@@ -27,112 +27,6 @@
 using namespace std;
 using namespace dlib;
 
-// void loadImages(const string& folder_name, 
-//     std::vector<matrix<rgb_pixel> > & training_images,
-//     std::vector<unsigned long>& training_labels, 
-//     std::vector<matrix<rgb_pixel> > & testing_images,
-//     std::vector<unsigned long>& testing_labels);
-// // int getdir (string dir, std::vector<string> &files);
-// void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images, 
-//     std::vector<unsigned long>& dlib_labels, unsigned long label);
-
-
-
-// void loadImages(const string& folder_name, 
-    
-//     std::vector<matrix<rgb_pixel> > & training_images,
-//     std::vector<unsigned long>& training_labels, 
-//     std::vector<matrix<rgb_pixel> > & testing_images,
-//     std::vector<unsigned long>& testing_labels) {
-
-//     std::vector<string> files = std::vector<string>();
-    
-//     string train_positive_folder = folder_name + "/train/1";
-//     string train_negative_folder = folder_name + "/train/0";
-//     string test_positive_folder  = folder_name + "/val/1";
-//     string test_negative_folder  = folder_name + "/val/0";
-
-//     std::vector<string> train_positive_files = std::vector<string>();
-//     std::vector<string> train_negative_files = std::vector<string>();
-//     std::vector<string> test_positive_files = std::vector<string>();
-//     std::vector<string> test_negative_files = std::vector<string>();
-    
-//     training_images = std::vector<matrix<rgb_pixel> >();
-//     loadImageFileIntoDLibImages_one_dir(train_positive_folder, training_images, training_labels, 1);
-//     loadImageFileIntoDLibImages_one_dir(train_negative_folder, training_images, training_labels, 0);
-
-//     // image_window my_window1(training_images[0], "image 0");
-//     // my_window1.wait_until_closed();
-
-//     // image_window my_window2(training_images[10], "image 10");
-//     // my_window2.wait_until_closed();
-
-//     testing_images = std::vector<matrix<rgb_pixel> >();
-//     loadImageFileIntoDLibImages_one_dir(test_positive_folder, testing_images, testing_labels, 1);
-//     loadImageFileIntoDLibImages_one_dir(test_negative_folder, testing_images, testing_labels, 0);  
-
-// }
-
-// void loadImageFileIntoDLibImages_one_dir(string folder, std::vector<matrix<rgb_pixel> >& dlib_images,
-//      std::vector<unsigned long>& dlib_labels, unsigned long label) {
-
-//     std::vector<string> files = std::vector<string>();
-//     getdir(folder, files);
-
-
-//     for(int i=0; i<files.size(); i++) {
-
-//         dlib::matrix<dlib::rgb_pixel> dlib_img;
-//         string filename = folder + "/" + files[i].c_str();
-        
-//         try{
-            
-//             cv::Mat cv_img = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
-//             if (cv_img.empty()) {
-//                 //cout << "failed reading image! The file name is " << filename << endl;
-//                 continue;
-//             }
-//             dlib::cv_image<dlib::rgb_pixel> tmp_img(cv_img);
-//             dlib::assign_image(dlib_img, tmp_img);
-            
-
-//             // load_image(img, filename);
-//             // dlib_images[i].sets_size(img.nr(), img.nc());
-//             //cout << files[i].c_str() << dlib_img.size() << endl;
-//             // assign_all_pixels(dlib_images[i], img.data);
-//             dlib_images.push_back(dlib_img);
-//             dlib_labels.push_back(label);
-//         }
-//         catch (exception& e)
-//         {
-//             cout << "exception thrown: " << e.what() << endl;
-//         }
-//     }
-
-
-//     cout << "images size is " << dlib_images.size() << endl;
-//     cout << "labels size is " << dlib_labels.size() << endl;
-
-
-
-// }
-
-// int getdir (string dir, std::vector<string> &files)
-// {
-//     DIR *dp;
-//     struct dirent *dirp;
-//     if((dp  = opendir(dir.c_str())) == NULL) {
-//         cout << "Error(" << errno << ") opening " << dir << endl;
-//         return errno;
-//     }
-
-//     while ((dirp = readdir(dp)) != NULL) {
-//         files.push_back(string(dirp->d_name));
-//     }
-//     closedir(dp);
-//     return 0;
-// }
-
 int main() {
 
     string input_dir = "/home/zhida/Documents/Data/road_sign_original_data";
@@ -155,28 +49,48 @@ int main() {
     // cout << training_labels.size() << endl;
 
     // define net
+
+    // best !!!
+    using net_type = loss_multiclass_log<
+                                fc<2,       
+                                relu<fc<84,   
+                                relu<fc<120,  
+                                max_pool<2,2,2,2,relu<con<16,5,5,1,1,
+                                max_pool<2,2,2,2,relu<con<6,5,5,1,1,
+                                input<matrix<rgb_pixel>> 
+                                >>>>>>>>>>>>;
+
     // using net_type = loss_multiclass_log<
     //                             fc<2,       
     //                             relu<fc<84,   
-    //                             relu<fc<120,  
+    //                             relu<fc<120, 
+    //                             max_pool<2,2,2,2,relu<con<28,5,5,1,1,
+    //                             max_pool<2,2,2,2,relu<con<6,5,5,1,1,
+    //                             input<matrix<rgb_pixel>> 
+    //                             >>>>>>>>>>>>;
+
+    // using net_type = loss_multiclass_log<
+    //                             fc<2,       
+    //                             relu<fc<42, 
+    //                             relu<fc<60,
     //                             max_pool<2,2,2,2,relu<con<16,5,5,1,1,
     //                             max_pool<2,2,2,2,relu<con<6,5,5,1,1,
     //                             input<matrix<rgb_pixel>> 
     //                             >>>>>>>>>>>>;
 
-    using net_type = loss_multiclass_log<
-                                fc<2,       
-                                relu<fc<84,   
-                                relu<fc<120, 
-                                max_pool<2,2,2,2,relu<con<28,5,5,1,1,
-                                max_pool<2,2,2,2,relu<con<6,5,5,1,1,
-                                input<matrix<rgb_pixel>> 
-                                >>>>>>>>>>>>;
+    // using net_type = loss_multiclass_log<
+    //                             fc<2,  
+    //                             relu<fc<300,
+    //                             max_pool<2,2,2,2,con<250,4,4,1,1,  
+    //                             max_pool<2,2,2,2,con<150,4,4,1,1,
+    //                             max_pool<2,2,2,2,con<100,7,7,1,1,
+    //                             input<matrix<rgb_pixel>> 
+    //                             >>>>>>>>>>;
 
     net_type net;
     
     dnn_trainer<net_type> trainer(net);
-    trainer.set_learning_rate(0.01);
+    trainer.set_learning_rate(0.001);
     trainer.set_min_learning_rate(0.00001);
     trainer.set_mini_batch_size(256);
     // trainer.set_mini_batch_size(512);
@@ -205,9 +119,9 @@ int main() {
         if (predicted_labels[i] == training_labels[i])
             ++num_right;
         else {
-            string name = "" + training_labels[i];
-            image_window my_window2(training_images[i], name);
-            my_window2.wait_until_closed();
+            // string name = "" + training_labels[i];
+            // image_window my_window2(training_images[i], name);
+            // my_window2.wait_until_closed();
             ++num_wrong;
         }
         
